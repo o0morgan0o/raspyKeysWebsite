f24bff261715eb45d4d85d9bec56b764e4670a27
o0morgan0o <morgan.thibert@protonmail.com>
2020-07-30 18:44:40 +0200

    Updates

---
+++ precache-manifest.ad01901f68b922fe9146651d97440e1b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0b761dcaf15083a10f436b3b756dc6",
+    "url": "/raspyKeysWebsite/index.html"
+  },
+  {
+    "revision": "534b320164fbda14a5c8",
+    "url": "/raspyKeysWebsite/static/css/main.e70b8e5b.chunk.css"
+  },
+  {
+    "revision": "aac7df5da5a99084c5d8",
+    "url": "/raspyKeysWebsite/static/js/2.8f062ebb.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/raspyKeysWebsite/static/js/2.8f062ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534b320164fbda14a5c8",
+    "url": "/raspyKeysWebsite/static/js/main.5771984a.chunk.js"
+  },
+  {
+    "revision": "0324654ec330656bbe8a",
+    "url": "/raspyKeysWebsite/static/js/runtime-main.122ea611.js"
+  },
+  {
+    "revision": "9adee72b9ccbe15e2c563f38b8ced023",
+    "url": "/raspyKeysWebsite/static/media/bg3.9adee72b.png"
+  },
+  {
+    "revision": "e8dd8b6077826233cc672c2edf1a3c29",
+    "url": "/raspyKeysWebsite/static/media/functionDrums.e8dd8b60.svg"
+  },
+  {
+    "revision": "88bbc9cc1f0d026a7f39cbd2e3169451",
+    "url": "/raspyKeysWebsite/static/media/functionEar.88bbc9cc.svg"
+  },
+  {
+    "revision": "af5fe4887ecd1336ce9650de7b1c699e",
+    "url": "/raspyKeysWebsite/static/media/functionRec.af5fe488.svg"
+  },
+  {
+    "revision": "5d8bc9bd0f9d3aa47dd78b5d47577dd1",
+    "url": "/raspyKeysWebsite/static/media/logo.5d8bc9bd.svg"
+  },
+  {
+    "revision": "c1b61addf7aec1845deef5f79de9d295",
+    "url": "/raspyKeysWebsite/static/media/part1.c1b61add.png"
+  },
+  {
+    "revision": "40f36b5e35d8b1a1767c43ad051261e5",
+    "url": "/raspyKeysWebsite/static/media/part2.40f36b5e.png"
+  },
+  {
+    "revision": "ebc8d92df20b6777bae667ac56760554",
+    "url": "/raspyKeysWebsite/static/media/part3.ebc8d92d.png"
+  },
+  {
+    "revision": "2d1256a3be9d7696b9f99f95d156db35",
+    "url": "/raspyKeysWebsite/static/media/piano.2d1256a3.svg"
+  },
+  {
+    "revision": "d088c776c8fa266ff595308fa0aa3a99",
+    "url": "/raspyKeysWebsite/static/media/plus.d088c776.svg"
+  },
+  {
+    "revision": "7bcfc0b1771e6c6aab9a4f11c90c03a0",
+    "url": "/raspyKeysWebsite/static/media/raspiAlone.7bcfc0b1.png"
+  },
+  {
+    "revision": "7cc450ac21111f1f3fe3871ab7ebc008",
+    "url": "/raspyKeysWebsite/static/media/raspiAlone2.7cc450ac.png"
+  },
+  {
+    "revision": "e08c63b438da38ac801709bed06785de",
+    "url": "/raspyKeysWebsite/static/media/raspiHeadPicCrop.e08c63b4.png"
+  },
+  {
+    "revision": "c04920078b3e802818e71b2d4764d55f",
+    "url": "/raspyKeysWebsite/static/media/triangle.c0492007.svg"
+  }
+]);
